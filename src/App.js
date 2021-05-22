e5303932226025d93eb7a0ee3281696c33b41716
JBoundless <39219944+JBoundless@users.noreply.github.com>
2021-05-21 20:14:04 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       return (
     <div>
       <AppBar />
-      <img src="https://media.npr.org/assets/img/2019/06/17/nprlogo_rgb_whiteborder_custom-7c06f2837fb5d2e65e44de702968d1fdce0ce748-s800-c85.png" style={{width: "25%", height: "100%"}} />
+      <img src="https://1000logos.net/wp-content/uploads/2017/07/Font-NPR-Logo.jpg" />
       <h1>Listen Live to NPR</h1>
       <p>Click on the live feed to listen to all NPR programs.</p>
       <h2>Now Playing</h2>
